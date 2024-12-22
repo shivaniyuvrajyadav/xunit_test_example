98192ec80393a53e349efaa10bb8da24aa692ac1
Admin <Admin@LAPTOP-V3GL5398>
2024-12-22 15:32:20 +0530

    Add project files.

---
+++ TestProject1/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xunit_test_example.Controllers;
+
+namespace TestProject1.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+           //AAA
+           //ARANGE
+           HomeController controller = new HomeController();
+            string expectedResult = "hii";
+
+            //ACT
+          string result =  controller.Index();
+           //ASSERT
+           Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ xunit_test_example/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xunit_test_example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    { 
+        public string Index ()
+        {
+            return "hii";
+        }
+    }
+}
